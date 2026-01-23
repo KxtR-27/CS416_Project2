4d94d725cbfa7faa6b9c5899058cac965dce238c
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-01-23 11:32:19 -0500

    Add GSON library as a dependency

--- src/config/ConfigSingleton.java
+++ src/config/ConfigSingleton.java
@@ -3,6 +3,15 @@ package config;
 // only a skeleton, so "unused" warning can be disregarded for now.
 @SuppressWarnings("unused")
 
-class ConfigSingleton {
+record ConfigSingleton(
+		ConfigDevice[] devices
+) {
 
 }
+
+record ConfigDevice(
+		String mac,
+		int port,
+		String ip,
+		String[] links
+) {}
