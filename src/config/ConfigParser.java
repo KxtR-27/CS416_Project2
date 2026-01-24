2101819d1b9e1884cf09fb8f2951332618ee1e7e
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-01-24 17:34:24 -0500

    Add and detail Javadoc comments for the entire `config` package

--- src/config/ConfigParser.java
+++ src/config/ConfigParser.java
@@ -12,56 +12,108 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Handles loading and parsing of the configuration files.
- * More Javadoc soon.
- * @author Kat
+ * Parses device configurations from a {@code config.json} file in the same directory.<br>
+ * <em>As a static utility class, this cannot be instanced.</em>
+ * <p>
+ * <b><em><u>If you are not a developer of this package, you only need to worry about {@code getConfigForDevice()}.</u></em></b>
+ * <p>
+ * The raw configuration loader uses Google Gson to read the {@code config.json} file.<br> From there, loaded
+ * information is parsed into a {@code Map} ({@code Hashmap}), as maps are optimized for getting and setting operations,
+ * which is what this class is for.
+ * <p>
+ * Every call to a public method also refreshes the map, so any changes made to the {@code config.json} file will
+ * reflect immediately.
+ *
+ * @author KxtR-27 (Kat)
+ * @see #getConfigForDevice(String)
+ * @see DeviceConfig
  */
+@SuppressWarnings("unused" /* because it's used later based on the rubric. */)
 class ConfigParser {
+	/**
+	 * The Gson object used in conjunction with a reader to read data from the {@code config.json} file.
+	 */
 	private static final Gson GSON = new Gson();
-	private static Map<String, DeviceConfig> configMap;
 
 	/**
-	 * Returns an object with a port, IP address, and list of neighbors, in that order.
-	 * The only method you need to worry about.
-	 * Calling this method more than once effectively refreshes the config.
-	 * More Javadoc soon.
-	 * @author KxtR-27
+	 * A parsed map of keys (device IDs) and values ({@code DeviceConfig}s)
+	 * that serves as a functional representation of the JSON structure itself.
+	 */
+	private static final Map<String, DeviceConfig> CONFIGS_MAP = new HashMap<>(7);
+
+	/**
+	 * Gets the configuration information for a virtual network device.
+	 * Calling this method also updates the parsed configuration,
+	 * so you can call this multiple times to pull potential changes to the config file.
+	 *
+	 * @param id ID/"MAC" for a host or switch device in the virtual network, (ex. "S1" or "A")
+	 *
+	 * @return a {@code DeviceConfig} object with the device's port, IP address, and neighbors,
+	 * 		<em>or <b>null</b> if no configuration exists for the ID.</em>
+	 *
 	 * @see DeviceConfig
 	 */
+	@SuppressWarnings("unused" /* because it's used later on based upon the rubric.*/)
 	public static DeviceConfig getConfigForDevice(String id) {
 		updateConfigMap();
-		return configMap.get(id);
+		return CONFIGS_MAP.get(id);
 	}
 
-
+	/**
+	 * Parses config file information into entries which are put into the config map.
+	 * Device IDs serve as keys, and the other device configuration information are encapsulated in the value.
+	 * This effectively "reloads" the {@code config.json} file and updates existing entries.
+	 *
+	 * @see #loadConfigFile()
+	 * @see RawDeviceConfig
+	 */
 	private static void updateConfigMap() {
-		Map<String, DeviceConfig> updatedMap = new HashMap<>();
 		RawDeviceConfig[] rawConfigs = loadConfigFile();
 
 		// abort update if something went wrong
-		if (rawConfigs == null)
+		if (rawConfigs == null) {
+			System.err.printf("Config file loaded, but valid configuration is not present.%n");
 			return;
+		}
 
 		for (RawDeviceConfig rawConfig : rawConfigs)
-			updatedMap.put(rawConfig.id, new DeviceConfig(rawConfig.port, rawConfig.ipAddress, rawConfig.neighbors));
-
-		configMap = updatedMap;
+			CONFIGS_MAP.put(
+					rawConfig.id, new DeviceConfig(
+							rawConfig.port,
+							rawConfig.ipAddress,
+							rawConfig.neighbors
+					)
+			);
 	}
 
+	/**
+	 * Using a JsonReader from gson, converts the {@code config.json}
+	 * file's JSON structure into an effectively identical array of objects.
+	 *
+	 * @return an array of {@code RawDeviceConfig}
+	 *
+	 * @see RawDeviceConfig
+	 */
 	private static RawDeviceConfig[] loadConfigFile() {
 		// try-with-resources automatically closes the readers after using them
 		try (JsonReader reader = new JsonReader(new FileReader("src/config/config.json"))) {
 			return GSON.fromJson(reader, RawDeviceConfig[].class);
 		}
 		catch (Exception e) {
-			printExtraErrorMessage(e);
-			e.printStackTrace(System.err);
-
+			printErrorWithMessage(e);
 			return null;
 		}
 	}
 
-	private static void printExtraErrorMessage(Exception e) {
+	/**
+	 * Prints a more human-readable indication of any error in {@code loadConfigFile()}, should an error occur.
+	 * Then prints the error stacktrace as well.
+	 *
+	 * @param e the caught exception to map to a message and print
+	 *
+	 * @see #loadConfigFile()
+	 */
+	private static void printErrorWithMessage(Exception e) {
 		String extraMessage = switch (e) {
 			case JsonIOException _ -> "Unable to read config file.";
 			case JsonSyntaxException _ -> "Could not correctly parse config file.";
@@ -74,11 +126,26 @@ class ConfigParser {
 		System.err.printf("%s%n", extraMessage);
 	}
 
-
+	/**
+	 * Used internally to map the config.json file's contents to an object of effectively identical form.
+	 * A RawDeviceConfig is later used to create a map entry for a parsed configuration structure.
+	 * For usable information, use a DeviceConfig record instead.
+	 * <p>
+	 * This class provides no functionality aside from storing data in a record to be parsed later.
+	 *
+	 * @param id        The ID/"MAC" of a host or switch device
+	 * @param port      The port on which the host or switch operates
+	 * @param ipAddress The IP address on which the host or switch operates
+	 * @param neighbors The "linked" host and switches of this device in the topology
+	 *
+	 * @see #loadConfigFile()
+	 * @see DeviceConfig
+	 */
 	private record RawDeviceConfig(
 			String id,
 			int port,
 			String ipAddress,
 			String[] neighbors
-	) {}
+	) {
+	}
 }
--- src/config/DeviceConfig.java
+++ src/config/DeviceConfig.java
@@ -3,10 +3,26 @@ package config;
 import java.util.Arrays;
 
 /**
- * Stores configuration information for network devices.
- * @param port
- * @param ipAddress
- * @param neighbors
+ * Stores configuration information for network devices except for the device ID.
+ * The device ID is used as a map key in the {@code ConfigParser} class instead.
+ * This is because the device ID must be provided in order to access the configuration
+ * as it is outlined in the rubric.
+ * <p>
+ * <br>
+ * <em>
+ * A record is a special data storage class that is immutable/final and public.
+ * It removes the need to write a boilerplate class with getters and constructors.
+ * <p>
+ * The attributes themselves are variables, but they are accessed with a method of the same name.
+ * For example, instead of using {@code myDeviceConfig.port},
+ * you might use {@code myDeviceConfig.port()} with parentheses.
+ * </em>
+ *
+ * @param port      The port on which the host or switch operates
+ * @param ipAddress The IP address on which the host or switch operates
+ * @param neighbors The "linked" host and switches of this device in the topology
+ *
+ * @see ConfigParser
  */
 public record DeviceConfig(
 		int port,
