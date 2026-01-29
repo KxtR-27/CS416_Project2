327f248f13191523eb09a6c56d4f37185531e51d
Brody Roth <brodyroth23@gmail.com>
2026-01-29 15:04:24 -0500

    Adding create and update switch tables to fulfill the requirement for a virtual switch table for each switch.

--- src/lan_switch/lan_switch.java
+++ src/lan_switch/lan_switch.java
@@ -9,7 +9,37 @@ public class lan_switch {
     private String ipAddress;
     private int listeningPort;
     private final Map<String, DeviceConfig> neighborConfigs = new HashMap<>();
+    private final Map<String, Integer> switchTable = new HashMap<>();
     private final Map<Integer, DeviceConfig> virtualPorts = new HashMap<>();
+
+    private void create_switch_table(String sourceIP, int port){
+        if(!switchTable.containsKey(sourceIP) || switchTable.get(sourceIP) != port){
+            if (!switchTable.containsKey(sourceIP)) {
+                System.out.println("NEW HOST LEARNED: " + sourceIP + " on Port " + port);
+            } else {
+                System.out.println("HOST MOVED: " + sourceIP + " moved to Port " + port);
+            }
+            switchTable.put(sourceIP, port);
+
+            display_switch_table();
+        }
+    }
+
+    private void update_switch_table(String sourceIP, int port){
+        System.out.println("Updating IP to: " + sourceIP + " to now port" + port);
+        switchTable.put(sourceIP, port);
+    }
+
+    public void display_switch_table(){
+        System.out.println("\n========= SWITCH TABLE (ID: " + this.id + ") =========");
+        System.out.printf("%-15s | %-10s%n", "Source IP/MAC", "Port");
+        System.out.println("-------------------------------------------");
+
+        for (Map.Entry<String, Integer> entry : switchTable.entrySet()) {
+            System.out.printf("%-15s | %-10d%n", entry.getKey(), entry.getValue());
+        }
+        System.out.println("===========================================\n");
+    }
 
     public lan_switch(String id){
         this.id = id;
@@ -33,11 +63,12 @@ public class lan_switch {
         }
     }
 
-    static void main(String[] args) {
-        if (args.length < 1){
+    public static void main(String[] args) {
+        if (args.length > 0){
             try{
                 String inputID = args[0];
                 lan_switch lanSwitch = new lan_switch(inputID);
+                lanSwitch.display_switch_table();
             }
             catch (NumberFormatException e){
                 System.err.println("Argument Must Be Device ID");
