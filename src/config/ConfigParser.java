97283a7fa43b8a811154334ea0cffa963924e773
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-02-20 11:44:20 -0500

    Add Javadoc comments to ConfigParser

--- src/config/ConfigParser.java
+++ src/config/ConfigParser.java
@@ -11,30 +11,65 @@ import java.util.Map;
 
 import static config.ConfigTypes.*;
 
+/// Parses configuration information for Hosts, Switches, and Routers from the `config.json` file.
+///
+/// @author KxtR-27 (Kat)
+/// @see ConfigTypes
 @SuppressWarnings("unused" /* because it's used later based on the rubric. */)
 public class ConfigParser {
-	/// The Gson object used in conjunction with a JsonReader to read data from the `config.json` file.
+	/// The `Gson` object used in conjunction with a `JsonReader` to read data from the `config.json` file.
 	private static final Gson GSON = new Gson();
 
+
+	/// A map of all `Host` objects from the `config.json` file.
+	/// Maps are optimized for accessing values.
 	private static final Map<String, HostConfig> hostsMap = new HashMap<>();
 
+	/// A map of all `Switch` objects from the `config.json` file.
+	/// Maps are optimized for accessing values.
 	private static final Map<String, SwitchConfig> switchesMap = new HashMap<>();
 
+	/// A map of all `Router` objects from the `config.json` file.
+	/// Maps are optimized for accessing values.
 	private static final Map<String, RouterConfig> routersMap = new HashMap<>();
 
 
+	/// Returns a record of configuration information for a `Host` device.
+	///
+	/// @param id the ID of the `Host` device (ex. "A")
+	///
+	/// @return a `HostConfig` object containing all information related to the device,
+	/// 		or null if no device with the ID exists.
+	///
+	/// @see HostConfig
 	public static HostConfig getHostConfig(String id) {
 		if (hostsMap.isEmpty())
 			parseConfig();
 		return hostsMap.get(id);
 	}
 
+	/// Returns a record of configuration information for a `Switch` device.
+	///
+	/// @param id the ID of the `Switch` device (ex. "S1")
+	///
+	/// @return a `SwitchConfig` object containing all information related to the device,
+	///  		or null if no device with the ID exists.
+	///
+	/// @see SwitchConfig
 	public static SwitchConfig getSwitchConfig(String id) {
 		if (switchesMap.isEmpty())
 			parseConfig();
 		return switchesMap.get(id);
 	}
 
+	/// Returns a record of configuration information for a `Router` device.
+	///
+	/// @param id the ID of the `Router` device (ex. "R1")
+	///
+	/// @return a `RouterConfig` object containing all information related to the device,
+	/// 		or null if no device with the ID exists.
+	///
+	/// @see RouterConfig
 	public static RouterConfig getRouterConfig(String id) {
 		if (routersMap.isEmpty())
 			parseConfig();
@@ -42,22 +77,43 @@ public class ConfigParser {
 	}
 
 
+	/// Returns a record of configuration information for a `Host` device.
+	///
+	/// @param virtualIP the virtual IP of the `Host` device (ex. "subnet1.A")
+	///
+	/// @return a `HostConfig` object containing all information related to the device,
+	/// 		or null if no device with the ID exists.
+	///
+	/// @see HostConfig
+	/// @see #getHostConfig(String)
 	public static HostConfig getHostConfigFromVIP(String virtualIP) {
 		String id = virtualIP.split("\\.")[1];
 		return getHostConfig(id);
 	}
 
+	/// Returns a record of configuration information for a `Router` device.
+	///
+	/// @param virtualIP the virtual IP of the `Host` device  (ex. "subnet1.R1")
+	///
+	/// @return a `RouterConfig` object containing all information related to the device,
+	/// 		or null if no device with the ID exists.
+	///
+	/// @see RouterConfig
+	/// @see #getRouterConfig(String)
 	public static RouterConfig getRouterConfigFromVIP(String virtualIP) {
 		String id = virtualIP.split("\\.")[1];
 		return getRouterConfig(id);
 	}
 
 
+	/// Parses the config.json file into
+	/// a map of Host objects, a map of Switch objects, and a map of Router objects..
 	private static void parseConfig() {
 		ConfigSnapshot configuration = readConfigFile();
 		populateDeviceMaps(configuration);
 	}
 
+	/// Maps `Host[]`, `Switch[]`, and `Router[]` arrays to maps using their IDs as keys.
 	private static void populateDeviceMaps(ConfigSnapshot configuration) {
 		hostsMap.clear();
 		switchesMap.clear();
@@ -71,6 +127,7 @@ public class ConfigParser {
 			routersMap.put(routerDevice.id(), routerDevice);
 	}
 
+	/// Returns a `ConfigSnapshot` object of identical structure to the `config.json` itself.
 	private static ConfigSnapshot readConfigFile() {
 		try (JsonReader reader = newConfigReader()) {
 			return GSON.fromJson(reader, ConfigSnapshot.class);
@@ -81,11 +138,16 @@ public class ConfigParser {
 		}
 	}
 
+	/// Creates a new `JsonReader` wrapped around a `FileReader` at the `config.json` file's expected path.
 	private static JsonReader newConfigReader() throws FileNotFoundException {
 		return new JsonReader(new FileReader("src/config/config.json"));
 	}
 
 
+	/// A record with identical structure to the `config.json` file.
+	/// This makes reading the file extremely easy.
+	///
+	/// @see ConfigParser#readConfigFile()
 	private record ConfigSnapshot(
 			HostConfig[] hosts,
 			SwitchConfig[] switches,
