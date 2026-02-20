2cce5685d7d48122ded80b7621a874506d70a2b3
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-02-20 11:14:53 -0500

    Implement config parsing

--- src/config/ConfigParser.java
+++ src/config/ConfigParser.java
@@ -1,8 +1,6 @@
 package config;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
-import com.google.gson.JsonSyntaxException;
 import com.google.gson.stream.JsonReader;
 
 import java.io.FileNotFoundException;
@@ -11,115 +9,87 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-/// Parses and retrieves device configurations from a `config.json` file in the same directory.
-///
-/// **Unless you are a developer, you should <u>_only_</u> need to use the static
-/// `getConfigForDevice()` method.**
-///
-/// @author KxtR-27 (Kat)
-/// @see #getConfigForDevice(String)
-/// @see DeviceConfig
+import static config.ConfigTypes.*;
+
 @SuppressWarnings("unused" /* because it's used later based on the rubric. */)
 public class ConfigParser {
 	/// The Gson object used in conjunction with a JsonReader to read data from the `config.json` file.
 	private static final Gson GSON = new Gson();
 
-	/// A parsed map of keys (device IDs) and values (`DeviceConfig`s)
-	/// that serves as a functional representation of the JSON structure itself.
-	private static final Map<String, DeviceConfig> CONFIGS_MAP = new HashMap<>(7);
+	private static final Map<String, HostConfig> hostsMap = new HashMap<>();
 
-	/// Gets the configuration information for a virtual network device.
-	///
-	/// Calling this method also updates the parsed configuration,
-	/// so you can call this multiple times to pull potential changes to the config file.
-	///
-	/// @param id ID/"MAC" for a host or lan_switch device in the virtual network, (ex. "S1" or "A")
-	///
-	/// @return a `DeviceConfig` object with the device's port, IP address, and neighbors,
-	/// 				_or **null** if no configuration exists for the ID used.<br>
-	/// 				<sup>If this method returns null, please ensure that the ID used
-	/// 				exists in the `config.json` file.</sup>_
-	///
-	/// @see DeviceConfig
-	@SuppressWarnings("unused" /* because it's used later on based upon the rubric.*/)
-	public static DeviceConfig getConfigForDevice(String id) {
-		updateConfigMap();
-		return CONFIGS_MAP.get(id);
+	private static final Map<String, SwitchConfig> switchesMap = new HashMap<>();
+
+	private static final Map<String, RouterConfig> routersMap = new HashMap<>();
+
+
+	public static HostConfig getHostConfig(String id) {
+		if (hostsMap.isEmpty())
+			parseConfig();
+		return hostsMap.get(id);
 	}
 
-	/// Parses config file information into entries which are put into the config map.
-	///
-	/// @see #loadConfigFile()
-	/// @see RawDeviceConfig
-	private static void updateConfigMap() {
-		// can be null if error occurs
-		RawDeviceConfig[] rawConfigs = loadConfigFile();
-
-		// do not update if error occurred
-		if (rawConfigs == null)
-			return;
-
-		for (RawDeviceConfig rawConfig : rawConfigs)
-			CONFIGS_MAP.put(
-					rawConfig.id, new DeviceConfig(
-							rawConfig.port,
-							rawConfig.ipAddress,
-							rawConfig.neighbors
-					)
-			);
+	public static SwitchConfig getSwitchConfig(String id) {
+		if (switchesMap.isEmpty())
+			parseConfig();
+		return switchesMap.get(id);
 	}
 
-	/// Converts the `config.json` file into an effectively identical array of objects.
-	///
-	/// @return an array of `RawDeviceConfig` objects
-	///
-	/// @see RawDeviceConfig
-	private static RawDeviceConfig[] loadConfigFile() {
-		// try-with-resources automatically closes the readers after using them
-		try (JsonReader reader = new JsonReader(new FileReader("src/config/config.json"))) {
-			return GSON.fromJson(reader, RawDeviceConfig[].class);
+	public static RouterConfig getRouterConfig(String id) {
+		if (routersMap.isEmpty())
+			parseConfig();
+		return routersMap.get(id);
+	}
+
+
+	public static HostConfig getHostConfigFromVIP(String virtualIP) {
+		String id = virtualIP.split("\\.")[1];
+		return getHostConfig(id);
+	}
+
+	public static RouterConfig getRouterConfigFromVIP(String virtualIP) {
+		String id = virtualIP.split("\\.")[1];
+		return getRouterConfig(id);
+	}
+
+
+	private static void parseConfig() {
+		ConfigSnapshot configuration = readConfigFile();
+		populateDeviceMaps(configuration);
+	}
+
+	private static void populateDeviceMaps(ConfigSnapshot configuration) {
+		hostsMap.clear();
+		switchesMap.clear();
+		routersMap.clear();
+
+		for (HostConfig hostDevice : configuration.hosts())
+			hostsMap.put(hostDevice.id(), hostDevice);
+		for (SwitchConfig switchDevice : configuration.switches())
+			switchesMap.put(switchDevice.id(), switchDevice);
+		for (RouterConfig routerDevice : configuration.routers())
+			routersMap.put(routerDevice.id(), routerDevice);
+	}
+
+	private static ConfigSnapshot readConfigFile() {
+		try (JsonReader reader = newConfigReader()) {
+			return GSON.fromJson(reader, ConfigSnapshot.class);
 		}
-		catch (Exception e) {
-			printErrorWithMessage(e);
-			return null;
+		catch (IOException e) {
+			System.err.printf("An unexpected error occurred while parsing the config file.%n");
+			throw new RuntimeException(e);
 		}
 	}
 
-	/// Prints the error with an additional help message.
-	///
-	/// @param e the caught exception to map to a message and print
-	///
-	/// @see #loadConfigFile()
-	private static void printErrorWithMessage(Exception e) {
-		String extraMessage = switch (e) {
-			case JsonIOException _jioe -> "Unable to read config file.";
-			case JsonSyntaxException _jse -> "Could not correctly parse config file.";
-			case FileNotFoundException _fnfe -> "Config file not found.";
-			case IOException _ioe -> "Unexpected file-related issue occurred.";
-
-			default -> "Unhandled exception occurred.";
-		};
-
-		System.err.printf("%s%n", extraMessage);
+	private static JsonReader newConfigReader() throws FileNotFoundException {
+		return new JsonReader(new FileReader("src/config/config.json"));
 	}
 
-	/// Used internally to map the `config.json` file's contents to an object of effectively identical form.
-	/// A `RawDeviceConfig` is later used to create a map entry for a parsed configuration structure.
-	///
-	/// For usable information, use a `DeviceConfig` record instead.
-	///
-	/// @param id        The ID/"MAC" of a host or lan_switch device
-	/// @param port      The port on which the host or lan_switch operates
-	/// @param ipAddress The IP address on which the host or lan_switch operates
-	/// @param neighbors The "linked" host and switches of this device in the topology
-	///
-	/// @see #loadConfigFile()
-	/// @see DeviceConfig
-	private record RawDeviceConfig(
-			String id,
-			int port,
-			String ipAddress,
-			String[] neighbors
+
+	private record ConfigSnapshot(
+			HostConfig[] hosts,
+			SwitchConfig[] switches,
+			RouterConfig[] routers
 	) {
 	}
 }
